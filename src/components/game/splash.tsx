8237edc3932fe94b85385c13296bf9c7a751c014
xiaoqin001 <xiaoqin6787@outlook.com>
2024-12-11 21:51:12 -0800

    updated game logic

--- src/components/game/splash.tsx
+++ src/components/game/splash.tsx
@@ -4,11 +4,15 @@ import { useContext } from "react";
 
 export default function Splash({ heading = "You won!", type = "" }) {
   const { startGame } = useContext(GameContext);
+  startGame
 
   return (
     <div className={`${styles.splash} ${type === "won" && styles.win}`}>
       <div>
         <h1>{heading}</h1>
+        <button className={styles.button} onClick={startGame}>
+          Submit to Leaderboard
+        </button>
         <button className={styles.button} onClick={startGame}>
           Play again
         </button>
