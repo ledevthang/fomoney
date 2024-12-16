525a3fd4f9b7f5d35d0b8a1e0a9ba3d8b602b8cd
ledevthang <ledevthang@gmail.com>
2024-12-16 09:47:19 +0700

    fix: update url game

--- src/services/submitGame.ts
+++ src/services/submitGame.ts
@@ -1,44 +1,41 @@
 /* eslint-disable no-console */
 
-
 // Function to submit the score
-export const submitScore = async (accessToken:string | null, score: number, season: string) => {
-    // const accessToken = useUserStore.getState().accessToken;
+export const submitScore = async (
+  accessToken: string | null,
+  score: number,
+  season: string,
+) => {
+  // const accessToken = useUserStore.getState().accessToken;
 
+  const url = "/api/update-point";
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  };
 
+  const body = JSON.stringify({
+    point: score,
+    season: season,
+  });
 
-    const url = '/api/update-point';
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`,
-    };
-
-    const body = JSON.stringify({
-        point: score,
-        season: season,
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: body,
     });
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: body,
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            alert('You have successfully submit your game!')
-            console.log('Score submitted successfully:', data);
-            return response.status;
-        } else {
-            // alert('Submit failed')
-
-            console.error('Error submitting score:', response.statusText);
-        }
-    } catch (error) {
-        // alert('Submit failed')
-
-        console.error('Request failed', error);
+    if (response.ok) {
+      const data = await response.json();
+      alert("You have successfully submit your game!");
+      console.log("Score submitted successfully:", data);
+      return response.status;
+    } else {
+      console.error("Error submitting score:", response.statusText);
     }
-    return;
+  } catch (error) {
+    console.error("Request failed", error);
+  }
+  return;
 };
