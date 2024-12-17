1bb4e1b958d2f22266c14252d09758e07ac81820
xiaoqin001 <xiaoqin6787@outlook.com>
2024-12-16 19:34:08 -0800

    fixed

--- src/services/submitGame.ts
+++ src/services/submitGame.ts
@@ -19,37 +19,30 @@ export const submitScore = async (
     season: season,
   });
 
+
+
   try {
     const response = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: headers,
       body: body,
     });
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: body,
-        });
+    if (response.ok) {
+      const data = await response.json();
+      // alert('You have successfully submit your game!')
+      console.log('Score submitted successfully:', data);
+      return response.status;
+    } else {
+      // alert('Submit failed')
 
-        if (response.ok) {
-            const data = await response.json();
-            // alert('You have successfully submit your game!')
-            console.log('Score submitted successfully:', data);
-            return response.status;
-        } else {
-            // alert('Submit failed')
-
-            console.error('Error submitting score:', response.statusText);
-        }
-    } catch (error) {
-        // alert('Submit failed')
-
-        console.error('Request failed', error);
+      console.error('Error submitting score:', response.statusText);
     }
   } catch (error) {
-    console.error("Request failed", error);
+    // alert('Submit failed')
+
+    console.error('Request failed', error);
   }
+
   return;
 };
